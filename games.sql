d2dfbb663ac79b366043f60055671032bf7038cb
CaptainObi <captainobi.mrt@gmail.com>
2021-08-11 11:04:19 +0100

    updated sql

--- games.sql
+++ games.sql
@@ -1,37 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 21, 2021 at 05:03 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `boardgamecompare`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `games`
---
-
-CREATE TABLE `games` (
-  `gameID` int(11) NOT NULL,
-  `ComplexElo` int(11) NOT NULL,
-  `DepthElo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE games (
+  gameID SERIAL,
+  ComplexElo INT NOT NULL,
+  DepthElo INT NOT NULL,
+  PRIMARY KEY (gameID)
+);
 
 --
 -- Indexes for dumped tables
@@ -40,9 +14,6 @@ CREATE TABLE `games` (
 --
 -- Indexes for table `games`
 --
-ALTER TABLE `games`
-  ADD PRIMARY KEY (`gameID`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
